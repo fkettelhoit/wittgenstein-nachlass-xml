7cd511d190d042ea46081c4cd7b0b73397e4da46
Frederic Kettelhoit <kettelhoit@gmail.com>
2020-03-23 23:53:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Wittgenstein's Nachlass as XML
+
+![Fetch Wittgenstein Nachlass](https://github.com/fkettelhoit/wittgenstein-nachlass-xml/workflows/Fetch%20Wittgenstein%20Nachlass/badge.svg)
+
+> The texts are made available by permission of The Master and Fellows of
+> Trinity College, Cambridge and the University of Bergen, Bergen, and in
+> agreement with Oxford University Press with whom WAB cooperates towards
+> producing a new Bergen Electronic Edition. Please pay special attention to
+> the fact that only the 5000 pages of the Discovery project items (Ts-201a1,
+> Ts-201a2, Ms-139a, Ts-207, Ms-114, Ms-115, Ms-153a, Ms-153b, Ms-154, Ms-155,
+> Ms-156a, Ms-148, Ms-149, Ms-150, Ts-212, Ts-213, p.39v of Ms-140, Ms-141,
+> Ms-152, Ts-310) are made available under the CCPL BY-NC 4.0 license. For all
+> other items standard copyright regulations and permission restrictions apply.
+> The copyright holders are: The Master and Fellows of Trinity College,
+> Cambridge; The University of Bergen, Bergen.
+
+For more details, see http://wab.uib.no/wab_transcriptions.page
+
+This repository periodically downloads, formats (using `xmllint --format`) and
+auto-commits the XML source files of Wittgenstein's Nachlass as transcribed and
+edited by the Wittgenstein Archives Bergen (WAB).
+
+Please note that while the software used to fetch the Nachlass is released
+under the MIT license, the Nachlass XML files are released under a Creative
+Commons license (see `xml/LICENSE`).
---
+++ bin/main.dart
@@ -0,0 +1,78 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io' as io;
+
+import 'package:html/parser.dart' as html;
+
+void main(List<String> arguments) => _fetchNachlass();
+
+final String _wabOAUrl = 'http://wab.uib.no/cost-a32_xml/';
+
+Future<void> _fetchNachlass() async {
+  final docs = await _fetchXmlNames(_wabOAUrl);
+  await Future.wait(docs.map(_fetchDocument).map(_formatDocument));
+}
+
+Future<io.File> _fetchDocument(String name, {bool skipExisting = false}) async {
+  final blacklist = {
+    'Ms-140,39v_OA.xml',
+    'Ts-212%20renamed_OA.xml',
+    'Ts-246_OA.xml',
+    'Ts-309-Stonborough_OA.xml',
+    'Ts-310-Redpath_OA.xml'
+  };
+  if (blacklist.contains(name)) return null;
+
+  final file = io.File('xml/$name');
+  if (skipExisting && await file.existsSync()) {
+    print('$name already exists, skipping download');
+    return null;
+  }
+  print('fetching $name');
+  await io.HttpClient()
+      .getUrl(Uri.parse('$_wabOAUrl$name'))
+      .then((request) => request.close())
+      .then<dynamic>((response) => response.pipe(file.openWrite()));
+  return file;
+}
+
+Future<void> _formatDocument(Future<io.File> file) async {
+  final f = await file;
+  if (f == null) return;
+
+  final content = await f.readAsString();
+  final zeroWidthSpace = '\u200B';
+  final onlySpaces = content
+      .replaceAll(RegExp(r'[\n\t\r]'), ' ')
+      .replaceAll(zeroWidthSpace, '');
+  await f.writeAsString(onlySpaces);
+  return io.Process.run('xmllint', ['--format', f.path]).then((result) {
+    f.writeAsString(result.stdout.toString());
+  });
+}
+
+Future<String> _readResponse(io.HttpClientResponse response) {
+  final completer = Completer<String>();
+  final contents = StringBuffer();
+  response.transform(utf8.decoder).listen(contents.write,
+      onDone: () => completer.complete(contents.toString()));
+  return completer.future;
+}
+
+Future<List<String>> _fetchXmlNames(String url) async {
+  final fetched = await _fetchHtml(url);
+  final doc = html.parse(fetched);
+  return doc
+      .getElementsByTagName('a')
+      .where((a) =>
+          a.text.endsWith('.xml') &&
+          !a.text.contains('missing') &&
+          !a.text.startsWith('LPA'))
+      .map((a) => a.attributes['href'])
+      .toList();
+}
+
+Future<String> _fetchHtml(String uri) => io.HttpClient()
+    .getUrl(Uri.parse(uri))
+    .then((request) => request.close())
+    .then<String>(_readResponse);
